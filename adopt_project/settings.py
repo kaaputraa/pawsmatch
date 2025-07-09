8a5dc3c839422d861bfe992d9970c48c1c1cbec6
Wijaya <deeka2684@gmail.com>
2025-07-09 11:49:37 +0800

    Initial working backend with JWT, PostgreSQL, and adoption API

--- adopt_project/settings.py
+++ adopt_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders'
     'adoption',
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
@@ -97,7 +99,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://web-production-f6535.up.railway.app'
+    'https://web-production-f6535.up.railway.app',
+    'https://hoppscotch.io',
 ]
 
 
